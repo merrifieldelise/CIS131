71c51f14bbb55b991078c31b68b1c708b28208b7
Elise <emerrifield@mail.pima.edu>
2025-06-12 22:07:33 -0700

    add notes within code

--- CIS131_M2LabMPG_MerrifieldElise.py
+++ CIS131_M2LabMPG_MerrifieldElise.py
@@ -9,18 +9,18 @@ gallons_total = 0
 
 
 while True:
-    #input statements
+    #input for gallons and immediate test for sentinel value
     gallons = float(input("Enter the gallons of gas used (-1 to end): "))
     if gallons == -1:
-        mpg_total = miles_total / gallons_total
+        mpg_total = miles_total / gallons_total #calculate overall MPG
         print(f"The overall average miles/gallon was {mpg_total}")
-        break
-
+        break #end loop
+    #2nd input for miles driven
     miles = float(input("Enter the miles driven: "))
     #calculate and print trip mgp
     trip_mpg = miles/gallons
     print(f"The miles/gallons for this tank was {trip_mpg}")
-    #create totals for next calculation
+    #create totals for calculation
     miles_total += miles
     gallons_total += gallons
 
