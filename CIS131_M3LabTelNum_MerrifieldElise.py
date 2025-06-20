921c1c1f09a9664ab331c8764d9c4b5ff8a5ffb2
Elise <emerrifield@mail.pima.edu>
2025-06-20 14:04:42 -0700

    create dictionary holding num+letter associations

--- CIS131_M3LabTelNum_MerrifieldElise.py
+++ CIS131_M3LabTelNum_MerrifieldElise.py
@@ -2,3 +2,17 @@
 #CIS 131
 #M3 lab Telephone Number Word Generator
 #Take user phone Number and use it to generate letter combinations possible
+
+
+#create a dictionary to store the number as a key and associate it with its list of letters
+letters_numbers = {
+    2:['A', 'B', 'C'],
+    3:['D', 'E', 'F'],
+    4:['G', 'H', 'I'],
+    5:['J', 'K', 'L'],
+    6:['M', 'N', 'O'],
+    7:['P', 'R', 'S'],
+    8:['T', 'U', 'V'],
+    9:['W', 'X', 'Y']
+}
+
