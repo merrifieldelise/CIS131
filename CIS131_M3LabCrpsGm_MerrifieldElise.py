ff938139e7599a261e3532c6588de8f661c9a591
Elise <emerrifield@mail.pima.edu>
2025-06-25 10:07:52 -0700

    created file and header

---
+++ CIS131_M3LabCrpsGm_MerrifieldElise.py
@@ -0,0 +1,6 @@
+#Author: Elise Merrifield
+#CIS 131
+#M3 Lab Analyzing a game of craps
+#take the outlined program in figure 4.2 of chapter 5 in the intro to python textbook and increase play count
+#as well as storing wins ans losses in a dictionary for further analysis
+
