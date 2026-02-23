c5cd2c9a16cc82d248ac571bef90ab7798cd5a63
Elise <emerrifield@mail.pima.edu>
2026-02-23 12:51:47 -0700

    set up file for Module 5 part 1 lab

---
+++ CIS131_M5LabModClassDat_MerrifieldElise.py
@@ -0,0 +1,6 @@
+'''
+Script: Lab, Modifying The Internal Data Representation of a Class
+Action: Modify the class to store the time as the total number of seconds since midnight
+Author: Elise Merrifield
+Date: 02/23/2026
+'''
